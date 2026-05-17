89764d7653ba50e374588c777b34a57daaf0cfc6
Luan Ribeiro <kacio.luan@gmail.com>
2026-05-17 22:48:05 +0100

    [feat] kill, sort and ls utilities

--- src/kill.c
+++ src/kill.c
@@ -1,8 +1,44 @@
+#define _POSIX_C_SOURCE 200809L
 #include "common.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+static void print_usage(void)
+{
+    printf("Uso: %s PID\n", program_name);
+    printf("Objetivo: Termina um processo ativo no sistema operacional.\n");
+    printf("Opções:\n");
+    printf("  -h    : apresenta esta ajuda e sai imediatamente\n");
+}
 
 int main(int argc, char** argv)
 {
-    (void)argc;
     program_name = argv[0];
-    return 0;
+
+    if (argc < 2)
+    {
+        usage("PID");
+    }
+
+    if (argv[1][0] == '-' && argv[1][1] == 'h')
+    {
+        print_usage();
+        return EXIT_SUCCESS;
+    }
+
+    pid_t pid = (pid_t)atoi(argv[1]);
+    if (pid <= 0)
+    {
+        error_msg("PID inválido");
+    }
+
+    if (kill(pid, SIGTERM) == -1)
+    {
+        die("kill");
+    }
+
+    return EXIT_SUCCESS;
 }
--- src/sort.c
+++ src/sort.c
@@ -1,8 +1,108 @@
 #include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    char* text;
+} Line;
+
+static void print_usage(void)
+{
+    printf("Uso: %s [OPÇÕES] [FICHEIRO]...\n", program_name);
+    printf("Objetivo: Ordena ficheiros de texto. Por padrão, ordena crescentemente e grava em FICHEIRO.sort.\n");
+    printf("Opções:\n");
+    printf("  -d    : ordenação decrescente\n");
+    printf("  -h    : apresenta esta ajuda e sai imediatamente\n");
+}
+
+static int compare_asc(const void* a, const void* b)
+{
+    return strcmp(((Line*)a)->text, ((Line*)b)->text);
+}
+
+static int compare_desc(const void* a, const void* b)
+{
+    return strcmp(((Line*)b)->text, ((Line*)a)->text);
+}
+
+static void sort_file(const char* filename, bool reverse)
+{
+    FILE* fp = fopen(filename, "r");
+    if (!fp)
+    {
+        warn(filename);
+        return;
+    }
+
+    Line* lines = NULL;
+    int count = 0;
+    char* l;
+
+    while ((l = line_read(fp)) != NULL)
+    {
+        lines = realloc(lines, sizeof(Line) * (count + 1));
+        lines[count].text = l;
+        count++;
+    }
+    fclose(fp);
+
+    qsort(lines, count, sizeof(Line), reverse ? compare_desc : compare_asc);
+
+    char out_name[1024];
+    snprintf(out_name, sizeof(out_name), "%s.sort", filename);
+    FILE* out_fp = fopen(out_name, "w");
+    if (!out_fp)
+    {
+        warn(out_name);
+    }
+    else
+    {
+        for (int i = 0; i < count; i++)
+        {
+            fprintf(out_fp, "%s\n", lines[i].text);
+        }
+        fclose(out_fp);
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        free(lines[i].text);
+    }
+    free(lines);
+}
 
 int main(int argc, char** argv)
 {
-    (void)argc;
     program_name = argv[0];
-    return 0;
+    bool reverse = false;
+    char opt;
+
+    while ((opt = next_option(argc, argv, "dh")) != '\0')
+    {
+        switch (opt)
+        {
+            case 'd':
+                reverse = true;
+                break;
+            case 'h':
+                print_usage();
+                return EXIT_SUCCESS;
+            default:
+                usage("[-d] [FICHEIRO]...");
+        }
+    }
+
+    if (opt_index >= argc)
+    {
+        usage("[-d] [FICHEIRO]...");
+    }
+
+    for (int i = opt_index; i < argc; i++)
+    {
+        sort_file(argv[i], reverse);
+    }
+
+    return EXIT_SUCCESS;
 }
